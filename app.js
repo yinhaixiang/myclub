0719634fedef8ff02705aedf47fadc58969f0809
yinhaixiang <yinhaixiang@qq.com>
2015-05-06 15:47:52 +0800

    'update'

---
+++ app.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var sessionstore = require('sessionstore');
+
+var routes = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+app.use(session({
+  store: sessionstore.createSessionStore(),
+  secret: 'haixiang',
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', routes.index);
+app.get('/login', routes.login);
+app.post('/login', routes.doLogin);
+app.get('/logout', routes.logout);
+app.get('/home', routes.home);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + server.address().port);
+});
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+exports.index = function (req, res) {
+  res.render('index', {title: 'Index'});
+};
+exports.login = function (req, res) {
+  res.render('login', {title: '用户登陆'});
+};
+exports.doLogin = function (req, res) {
+  var user = {
+    username: 'admin',
+    password: 'admin'
+  }
+  if (req.body.username === user.username && req.body.password === user.password) {
+    res.redirect('/home');
+  }
+  res.redirect('/login');
+};
+exports.logout = function (req, res) {
+  res.redirect('/');
+};
+exports.home = function (req, res) {
+  var user = {
+    username: 'admin',
+    password: 'admin'
+  }
+  res.render('home', {title: 'Home', user: user});
+};
